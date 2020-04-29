902ade43a37e498a38d852f7f3db62bf4ad7560b
Chris Archibald <carchi8py@gmail.com>
2020-04-29 19:19:54 -0400

    Sync bitbucket and Github

---
+++ ansible_collections/netapp/um_info/README.md
@@ -0,0 +1,30 @@
+=============================================================
+
+ netapp.um_info
+
+ NetApp Unified Manager(AIQUM 9.7/OCUM) Collection
+
+ Copyright (c) 2020 NetApp, Inc. All rights reserved.
+ Specifications subject to change without notice.
+
+=============================================================
+# Installation
+```bash
+ansible-galaxy collection install netapp.um_info
+```
+To use Collection add the following to the top of your playbook, with out this you will be using Ansible 2.9 version of the module
+```
+collections:
+  - netapp.um_info
+```
+# Need help
+Join our Slack Channel at [Netapp.io](http://netapp.io/slack)
+
+# Release Notes
+
+## 20.5.0
+
+### New Modules
+- na_um_list_aggregates: list aggregates.
+- na_um_list_clusters: list clusters.
+- na_um_list_nodes: list nodes.
---
+++ ansible_collections/netapp/um_info/tests/unit/plugins/modules/test_na_um_list_clusters.py
@@ -0,0 +1,120 @@
+# (c) 2020, NetApp, Inc
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+""" unit tests for Ansible module: na_um_list_clusters """
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+import json
+import pytest
+
+from ansible_collections.netapp.um_info.tests.unit.compat import unittest
+from ansible_collections.netapp.um_info.tests.unit.compat.mock import patch, Mock
+from ansible.module_utils import basic
+from ansible.module_utils._text import to_bytes
+
+from ansible_collections.netapp.um_info.plugins.modules.na_um_list_clusters\
+    import NetAppUMCluster as my_module  # module under test
+
+
+# REST API canned responses when mocking send_request
+SRR = {
+    # common responses
+    'empty_good': ({}, None),
+    'end_of_sequence': (None, "Unexpected call to send_request"),
+    'generic_error': (None, "Expected error"),
+    # module specific responses
+    'get_cluster': {'name': 'ansible'}
+}
+
+
+def set_module_args(args):
+    """prepare arguments so that they will be picked up during module creation"""
+    args = json.dumps({'ANSIBLE_MODULE_ARGS': args})
+    basic._ANSIBLE_ARGS = to_bytes(args)  # pylint: disable=protected-access
+
+
+class AnsibleExitJson(Exception):
+    """Exception class to be raised by module.exit_json and caught by the test case"""
+    pass
+
+
+class AnsibleFailJson(Exception):
+    """Exception class to be raised by module.fail_json and caught by the test case"""
+    pass
+
+
+def exit_json(*args, **kwargs):  # pylint: disable=unused-argument
+    """function to patch over exit_json; package return data into an exception"""
+    if 'changed' not in kwargs:
+        kwargs['changed'] = False
+    raise AnsibleExitJson(kwargs)
+
+
+def fail_json(*args, **kwargs):  # pylint: disable=unused-argument
+    """function to patch over fail_json; package return data into an exception"""
+    kwargs['failed'] = True
+    raise AnsibleFailJson(kwargs)
+
+
+class MockUMConnection(object):
+    ''' mock server connection to Unified Manager host '''
+
+    def __init__(self):
+        ''' pass init '''
+
+
+class TestMyModule(unittest.TestCase):
+    ''' a group of related Unit Tests '''
+
+    def setUp(self):
+        self.mock_module_helper = patch.multiple(basic.AnsibleModule,
+                                                 exit_json=exit_json,
+                                                 fail_json=fail_json)
+        self.mock_module_helper.start()
+        self.addCleanup(self.mock_module_helper.stop)
+        self.server = MockUMConnection()
+        # whether to use a mock or a simulator
+        self.onbox = False
+
+    def set_default_args(self):
+        if self.onbox:
+            hostname = '10.10.10.10'
+            username = 'admin'
+            password = 'password'
+        else:
+            hostname = 'hostname'
+            username = 'username'
+            password = 'password'
+        return dict({
+            'hostname': hostname,
+            'username': username,
+            'password': password,
+        })
+
+    def test_module_fail_when_required_args_missing(self):
+        ''' required arguments are reported as errors '''
+        with pytest.raises(AnsibleFailJson) as exc:
+            set_module_args({})
+            my_module()
+        print('Info: %s' % exc.value.args[0]['msg'])
+
+    @patch('ansible_collections.netapp.um_info.plugins.modules.na_um_list_clusters.NetAppUMCluster.get_clusters')
+    def test_ensure_list_cluster_get_called(self, get_cluster):
+        ''' fetching details of cluster '''
+        set_module_args(self.set_default_args())
+        my_obj = my_module()
+        my_obj.server = self.server
+        my_obj.get_cluster = Mock(return_value=SRR['get_cluster'])
+        with pytest.raises(AnsibleExitJson) as exc:
+            my_obj.apply()
+        assert exc.value.args[0]['changed']
+        # to reset na_helper from remembering the previous 'changed' value
+
+    @patch('ansible_collections.netapp.um_info.plugins.modules.na_um_list_clusters.NetAppUMCluster.get_clusters')
+    def test_ensure_get_called_existing(self, get_cluster):
+        ''' test for existing cluster'''
+        set_module_args(self.set_default_args())
+        my_obj = my_module()
+        my_obj.get_cluster = Mock(return_value=SRR['get_cluster'])
+        assert my_obj.get_cluster() is not None
